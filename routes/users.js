e2e95c02ea6f99c49026fb57c04edc988ac0c722
Dhananjay <dhananjayman11@gmail.com>
2021-11-06 12:45:38 +0530

    mongoDB models

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,6 @@
 const router = require('express').Router();
 
-router.get('/usertest', (req, res) => {
-    res.send('user test -> 1');
-})
 
-router.post('/user-post-test', (req, res) => {
-    const username = req.body.username;
-    console.log(username);
-
-    res.json(username);
-})
 
 
 module.exports = router;
