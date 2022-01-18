a1424edfc3a7f95541ddd9e7d485e62113e52981
Dhananjay <dhananjayman11@gmail.com>
2022-01-18 13:47:10 +0530

    sort by time

--- routes/order.js
+++ routes/order.js
@@ -81,7 +81,7 @@ router.get("/find/:userId", verifyTokenAndAuth, async (req, res) => {
 
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
   try {
-    const orders = await Order.find();
+    const orders = await Order.find().sort({ createdAt: -1 });
     res.status(200).json(orders);
   } catch (err) {
     console.error(error);
