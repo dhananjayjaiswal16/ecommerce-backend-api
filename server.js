7d2a1f7bec315ef2482fdab68c3cf72cf233494e
Dhananjay <dhananjayman11@gmail.com>
2022-01-22 19:11:19 +0530

    env

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 dotenv.config();
 const app = express();
 
-mongoose.connect("mongodb+srv://dhananjay16:Digvijay18@cluster0.dx0so.mongodb.net/ecommerce?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     console.log("database connected...")
   })
